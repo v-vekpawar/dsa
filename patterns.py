4de3071d0d0fb9fad5fcf5fe02362e5ac0a8c625
ved <v.vekp4w4r@gmail.com>
2026-05-08 11:08:26 +0530

    Solved more pattern problems

--- patterns.py
+++ patterns.py
@@ -71,18 +71,66 @@ def p8(n):
         print()
 
 # ----9----
+def p9(n):
+    p7(n)
+    p8(n)
 
+# ----x----
+def px(n):
+    for i in range((n//2)+1):
+        for j in range(n-i-1):
+            print(" ",end="")
+        for k in range(2*i+1):
+            print("*",end="")
+        for l in range(n-i-1):
+            print(" ",end="")
+        print()
+    for i in range((n//2),0,-1):
+        for j in range(n-i):
+            print(" ",end="")
+        for k in range(2*i-1):
+            print("*",end="")
+        for l in range(n-i):
+            print(" ",end="")
+        print()
 
 # ----10----
+def p10_1(n):
+    for i in range(n):
+        for j in range(i+1):
+            print("*",end="")
+        print()
+    for i in range(n-1):
+        for j in range(n-i-1):
+            print("*",end="")
+        print()
 
+def p10_2(n):
+    for i in range(1,2*n):
+        stars=i if i<=n else 2*n-i
+        for j in range(stars):
+            print("*",end="")
+        print()
 
 # ----11----
-
+def p11(n):
+    for i in range(n):
+        start=1 if i%2==0 else 0
+        for j in range(i+1):
+            print(start,end="")
+            start=1-start
+        print()
 
 # ----12----
-
-
-
+def p12(n):
+    for i in range(n):
+        for j in range(i+1):
+            print(j+1,end="")
+        for k in range((2*(n-i-1)),0,-1):
+            print(" ",end="")
+        for l in range(i+1,0,-1):
+            print(l,end="")
+        print()
 
 def run_all(n):
     print("Pattern 1.0")
@@ -114,6 +162,21 @@ def run_all(n):
     print()
     print("Pattern 8")
     p8(n)
+    print()
+    print("Pattern 9")
+    p9(n)
+    print()
+    print("Pattern 10.1")
+    p10_1(n)
+    print()
+    print("Pattern 10.2")
+    p10_2(n)
+    print()
+    print("Pattern 11")
+    p11(n)
+    print()
+    print("Pattern 12")
+    p12(n)
     print()
 
 # if __name__ == "__main__":
